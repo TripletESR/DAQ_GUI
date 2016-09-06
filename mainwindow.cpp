fff2400d8fa92842eff886fff14ec764e638b8e8
Ryan@Triplet-ESR <ryantang@cns.s.u-tokyo.ac.jp>
2016-09-06 18:30:36 +0900

    improved graph ploting

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -72,13 +72,11 @@ void MainWindow::on_pushButton_clicked()
     int ch = ui->spinBox_ch->value();
     GetDataAndPlot(customPlot, ch);
 
-    //qDebug() << "Ready?" << oscui->osc->isReady();
 }
 
 void MainWindow::GetDataAndPlot(QCustomPlot *Plot, int ch){
 
     oscui->osc->GetData(ch, ui->spinBox_count->value(),oscui->osc->acqFlag);
-    //oscui->osc->GetData2(ch, ui->spinBox_count->value());
 
     if( Plot->graphCount() == 0){
         Plot->xAxis->setLabel("time [us]");
@@ -87,21 +85,24 @@ void MainWindow::GetDataAndPlot(QCustomPlot *Plot, int ch){
         Plot->yAxis->setRange(oscui->osc->yMin * 2, oscui->osc->yMax * 2);
     }
 
+    double plotyMax = Plot->yAxis->range().upper;
     double plotyMin = Plot->yAxis->range().lower;
+    if( oscui->osc->yMin * 2 < plotyMin ) plotyMin = oscui->osc->yMin * 2;
+    if( oscui->osc->yMax * 2 > plotyMax ) plotyMax = oscui->osc->yMax * 2;
+    Plot->yAxis->setRange(plotyMin, plotyMax);
 
-    if( Plot->graphCount() < ch) {
+    while(Plot->graphCount() < ch){
         Plot->addGraph();
-
-        switch (ch) {
-        case 1:Plot->graph(ch-1)->setPen(QPen(Qt::blue)); break;
-        case 2:Plot->graph(ch-1)->setPen(QPen(Qt::red)); break;
-        case 3:Plot->graph(ch-1)->setPen(QPen(Qt::darkGreen)); break;
-        case 4:Plot->graph(ch-1)->setPen(QPen(Qt::magenta)); break;
-        }
     }
-    Plot->graph(ch-1)->setData(oscui->osc->xData[ch], oscui->osc->yData[ch]);
-        //qDebug("%f, %f \n", oscui->osc->xMin, oscui->osc->xMax);
 
+    switch (ch) {
+    case 1:Plot->graph(ch-1)->setPen(QPen(Qt::blue)); break;
+    case 2:Plot->graph(ch-1)->setPen(QPen(Qt::red)); break;
+    case 3:Plot->graph(ch-1)->setPen(QPen(Qt::darkGreen)); break;
+    case 4:Plot->graph(ch-1)->setPen(QPen(Qt::magenta)); break;
+    }
+
+    Plot->graph(ch-1)->setData(oscui->osc->xData[ch], oscui->osc->yData[ch]);
 
     Plot->replot();
 }

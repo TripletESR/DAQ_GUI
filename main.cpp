189be86a446394aa4f62eb9b16a7825938d8834c
Ryan@windows10 <goluckyryan@gmail.com>
2016-12-03 17:55:26 +0900

    Prevent Multiple instance.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //prevent multiple instance
+    QSharedMemory sharedMemory;
+    sharedMemory.setKey("DAQ_GUI-Key");
+    if( sharedMemory.create(1) == false){
+        QMessageBox::warning(NULL, "Warning!", "Another instance alredy running!");
+        a.exit;
+        return 0;
+    }
+
     MainWindow w;
     w.show();
     return a.exec();

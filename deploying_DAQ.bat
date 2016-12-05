c8b76c8cedb7f11f0beff84688201a5287824eb2
Ryan@windows10 <goluckyryan@gmail.com>
2016-12-05 16:40:21 +0900

    improve bat script

--- deploying_DAQ.bat
+++ deploying_DAQ.bat
@@ -2,21 +2,27 @@
 title Deploying DAQ_GUI
 echo =====================================================
 echo This is batch script for deploying DAQ_GUI in winodws
+Set desktop=%USERPROFILE%\Desktop
+echo Desktop PATH: %Desktop%
 echo =====================================================
 pause
 
 Set name=DAQ_GUI.exe
-Set origin=C:\Users\Triplet-ESR\Desktop\build-DAQ_GUI-Desktop_Qt_5_7_0_MinGW_32bit-Release\release
-Set destination=C:\Users\Triplet-ESR\Desktop\DAQ_UI_release\
+Set origin=%desktop%\build-DAQ_GUI-Desktop_Qt_5_7_0_MinGW_32bit-Release\release
+Set destination=%desktop%\DAQ_UI_release\
 Set compiler=C:\Qt\5.7\mingw53_32\bin\
 
 rem -----------------------------------------
+echo =====================================================
 echo Copy *.exe to %destination%
+echo =====================================================
 xcopy %origin%\%name% %destination%
 pause
 
 rem -----------------------------------------
-set /p deployFlag="Deploy: Y/N?"
+echo =====================================================
+set /p deployFlag="  Deploy Application? (Y/N)"
+echo =====================================================
 
 IF "%deployFlag%"=="Y" (
 	rem -----------------------------------------
